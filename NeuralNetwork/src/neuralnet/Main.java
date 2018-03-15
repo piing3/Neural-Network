86cbd1641256515394f29be61b11b3257101ba20
piing3 <dhblue015@gmail.com>
2018-03-15 13:24:08 -0700

    NeuralNet and Main

---
+++ NeuralNetwork/src/neuralnet/Main.java
@@ -0,0 +1,16 @@
+package neuralnet;
+
+/**
+ * Project main
+ * @author Davin Holmberg
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
--- NeuralNetwork/src/neuralnet/NeuralNet.java
+++ NeuralNetwork/src/neuralnet/NeuralNet.java
@@ -1,16 +1,31 @@
 package neuralnet;
 
+import java.util.ArrayList;
+import neuralnet.math.IActivationFunction;
+
 /**
- * The main class for the ANN
- * @author dhblu
+ * The master class of the ANN
+ * @author Davin
  */
 public class NeuralNet {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    private InputLayer inputLayer;
+    private ArrayList<HiddenLayer> hiddenLayer;
+    private OutputLayer outputLayer;
+    private int numberOfHiddenLayers;
+    private int numberOfInputs;
+    private int numberOfOutputs;
+    private ArrayList<Double> input;
+    private ArrayList<Double> output;
+ 
+    public NeuralNet(int numberofinputs,int numberofoutputs,
+            int[] numberofhiddenneurons, IActivationFunction[] hiddenAcFnc, 
+            IActivationFunction outputAcFnc){
         
+        this.input = new ArrayList<>(numberOfInputs);
+        this.inputLayer = new InputLayer(numberofinputs);
+        
+        this.hiddenLayer.set(0, new HiddenLayer(inputLayer.getNumberOfNeuronsInLayer(), numberofhiddenneurons[0], hiddenAcFnc[0]));
     }
     
 }
